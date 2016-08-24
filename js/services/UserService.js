ecf19e2d4ca59a7634e74bf2297fe4fa06ca9779
mmoultrie <mmoultrie@gmail.com>
2016-08-24 15:23:56 -0400

    Removing unecessary stuff

--- js/services/UserService.js
+++ js/services/UserService.js
@@ -5,14 +5,17 @@ app.service('UserService',function (config,$http) {
     function getUrl() {
         return config.endpointUri + path;
     }
+
     service.all = function() {
        return $http.get(getUrl()).then(function(list) {
-           return list;
+           return list.data;
        });
     };
+
     service.update = function(obj) {
         return $http.put(getUrl(), obj);
     };
+
     service.create = function (obj) {
         return $http.post(getUrl(), obj);
     };
